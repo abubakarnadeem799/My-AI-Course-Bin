072684cb98a2729ba851383f86185f59815d9dcd
abubakarnadeem799 <abubakarnadeem799@gmail.com>
2026-02-01 11:24:16 +0500

    My Code

---
+++ Case3-HelloMe.py
@@ -0,0 +1,11 @@
+# First program
+print ("Starts here")
+print ("hello", "world", "Pak", "USA",sep="@", end="#")
+
+name = input("Please enter your name: ")
+
+print("Hello - welocme - " + name)
+
+print("ends here")
+
+a
