95dd5cd680cf350d08420fa6fe999c0a2fc1b75c
abubakarnadeem799 <abubakarnadeem799@gmail.com>
2026-03-07 11:14:52 +0500

    Updated Code
    Apply Multiple function For Practice

---
+++ My Tuple Practice.py
@@ -0,0 +1,1 @@
+print("Welocome to my Tuple Practice")
--- Python List Practice.py
+++ Python List Practice.py
@@ -9,3 +9,17 @@ print(Customer_List)
 
 Customer_List.insert(1, "Tahran")
 print(Customer_List)
+
+print(Customer_List[1])
+print(Customer_List[2])
+print(Customer_List)
+print(type(Customer_List[2]))
+
+Customer_List.remove(200)
+print(Customer_List)
+
+Customer_List.pop(1)
+print(Customer_List)
+
+Customer_List[0]= "My Change Value"
+print(Customer_List)
