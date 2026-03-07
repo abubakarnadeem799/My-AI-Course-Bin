c22c3f110b332db78dcd385f3ca45852a9fac311
abubakarnadeem799 <abubakarnadeem799@gmail.com>
2026-03-07 11:39:32 +0500

    Updated Code
    Practice some Functions

---
+++ My Dictionary Practice.py
@@ -0,0 +1,12 @@
+print("Welcome to my Dictionary Practice")
+
+dictionarycustomer = {"Name" : "Hamza", "Zip": 52201, "Price" : 600.70}
+print(dictionarycustomer)
+print(type(dictionarycustomer))
+
+dictionarycustomer["Name"] = "Hamza Ali bakhtawar"
+print(dictionarycustomer)
+
+dictionarycustomer["City"] = "Sailkot"
+
+print(dictionarycustomer)
--- My Tuple Practice.py
+++ My Tuple Practice.py
@@ -1,1 +1,15 @@
 print("Welocome to my Tuple Practice")
+
+Tuple_Customer = ("Trumpo", 100, 100.10, True)
+print(Tuple_Customer)
+print(Tuple_Customer[1])
+
+#Tuple_Customer[1] = "MY CHANGE VALUE"#
+
+for item in Tuple_Customer:
+    print(item)
+    
+
+
+
+
