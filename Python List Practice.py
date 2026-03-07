5467cfd3e4ea98491309e57bde671216136d9471
abubakarnadeem799 <abubakarnadeem799@gmail.com>
2026-03-07 10:55:04 +0500

    List_Updated
    New String insert

--- Python List Practice.py
+++ Python List Practice.py
@@ -2,3 +2,7 @@ print("Welcome to my Python Practice")
 
 Customer_List = ["Trump", 200, 0.0200, True ]
 print(Customer_List)
+print(type(Customer_List))
+
+Customer_List.append("Ayotollah ALi Khaninae")
+print(Customer_List)
